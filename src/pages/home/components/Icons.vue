f44f4e38008cf8e8cc62a72e0f53a99bf93ce227
zhaipeihan <zhaipeihan@outlook.com>
2018-05-25 22:43:52 +0800

    add icons

--- src/pages/home/components/Icons.vue
+++ src/pages/home/components/Icons.vue
@@ -1,93 +1,127 @@
 <template>
   <div class='icons'>
-    <div class='icon'>
-      <div class='icon-img'>
-        <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png"/>
-      </div>
-      <p class='icon-desc'>景点门票</p>
-    </div>
-    <div class='icon'>
-      <div class='icon-img'>
-        <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png"/>
-      </div>
-      <p class='icon-desc'>景点门票</p>
-    </div>
-    <div class='icon'>
-      <div class='icon-img'>
-        <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png"/>
-      </div>
-      <p class='icon-desc'>景点门票</p>
-    </div>
-    <div class='icon'>
-      <div class='icon-img'>
-        <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png"/>
-      </div>
-      <p class='icon-desc'>景点门票</p>
-    </div>
-    <div class='icon'>
-      <div class='icon-img'>
-        <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png"/>
-      </div>
-      <p class='icon-desc'>景点门票</p>
-    </div>
-    <div class='icon'>
-      <div class='icon-img'>
-        <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png"/>
-      </div>
-      <p class='icon-desc'>景点门票</p>
-    </div>
-    <div class='icon'>
-      <div class='icon-img'>
-        <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png"/>
-      </div>
-      <p class='icon-desc'>景点门票</p>
-    </div>
-    <div class='icon'>
-      <div class='icon-img'>
-        <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png"/>
-      </div>
-      <p class='icon-desc'>景点门票</p>
-    </div>
+    <swiper :options="swiperOption">
+      <swiper-slide v-for='(page,index) in pageIcons' :key="index">
+        <div class='icon' v-for='(item) in page' :key='item.id'>
+          <div class='icon-img'>
+            <img class="icon-img-content" :src="item.imgUrl"/>
+          </div>
+          <p class='icon-desc'>{{item.imgDesc}}</p>
+        </div>
+      </swiper-slide>
+    </swiper>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HomeIcons'
+  name: 'HomeIcons',
+  data () {
+    return {
+      swiperOption: {
+      },
+      iconList: [
+        {
+          id: '0001',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+          imgDesc: '景点门票'
+        },
+        {
+          id: '0002',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/ff/fdf170ee89594b02.png',
+          imgDesc: '必游榜单'
+        },
+        {
+          id: '0003',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/96/c70f1e85ae4a4f02.png',
+          imgDesc: '自然风光'
+        },
+        {
+          id: '0004',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/5a/13ceb38dcf262f02.png',
+          imgDesc: '一日游'
+        },
+        {
+          id: '0005',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/bd/9f7b9b2b60c1502.png',
+          imgDesc: '踏青赏花'
+        },
+        {
+          id: '0006',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/8246f27355943202.png',
+          imgDesc: '游乐场'
+        },
+        {
+          id: '0007',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/20/831d62d2e1c7be02.png',
+          imgDesc: '名胜古迹'
+        },
+        {
+          id: '0008',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+          imgDesc: '景点门票'
+        },
+        {
+          id: '0009',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+          imgDesc: '景点门票'
+        },
+        {
+          id: '0010',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+          imgDesc: '景点门票'
+        }
+      ]
+    }
+  },
+  computed: {
+    pageIcons () {
+      let pages = []
+      this.iconList.forEach((item, index) => {
+        let page = Math.floor(index / 8)
+        if (!pages[page]) {
+          pages[page] = []
+        }
+        pages[page].push(item)
+      })
+      return pages
+    }
+  }
 }
 </script>
 
 <style lang="stylus" scoped>
-  .icons
-    overflow: hidden
+  @import '~@/assets/styles/mixins.styl'
+  .iconsv >>> .swiper-container
     height: 0
     padding-bottom: 50%
-    .icon
-      position relative
-      overflow hidden
-      float: left
-      width: 25%
-      height: 0
-      padding-bottom: 25%
-      .icon-img
-        position: absolute
-        top: 0
-        left: 0
-        right: 0
-        bottom: .44rem
-        box-sizing: border-box
-        padding: .1rem
-        .icon-img-content
-          display: block
-          margin: 0 auto
-          height: 100%
-      .icon-desc
-        position: absolute
-        bottom: 0
-        left: 0
-        right: 0
-        line-height: .44rem
-        height: .44rem
-        color: #333
-        text-align: center
+  .icon
+    position relative
+    overflow hidden
+    float: left
+    width: 25%
+    height: 0
+    padding-bottom: 25%
+    .icon-img
+      position: absolute
+      top: 0
+      left: 0
+      right: 0
+      bottom: .44rem
+      box-sizing: border-box
+      padding: .1rem
+      .icon-img-content
+        display: block
+        margin: 0 auto
+        height: 100%
+    .icon-desc
+      position: absolute
+      bottom: 0
+      left: 0
+      right: 0
+      line-height: .44rem
+      height: .44rem
+      color: #333
+      text-align: center
+      ellipsis()
 </style>
