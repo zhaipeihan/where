f603d85b1199e3c4cd0c4e057eb2ce80c4af2995
zhaipeihan <zhaipeihan@outlook.com>
2018-05-25 15:00:44 +0800

    project init

---
+++ src/pages/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div class='home'>Home</div>
+    <router-link to='/list' class="home">列表页</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style scoped>
+  .home {
+    font-size: 50px
+  }
+</style>
---
+++ src/pages/List.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class='list'>List</div>
+</template>
+
+<script>
+export default {
+  name: 'List'
+}
+</script>
+
+<style scoped>
+  .list {
+    font-size: 50px
+  }
+</style>
