87160b8f58b0cd4422c30351d538fdb8e9e116a1
zhaipeihan <zhaipeihan@outlook.com>
2018-05-26 02:25:56 +0800

    add recommend

---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,86 @@
+<template>
+  <div>
+    <div class='recommend-title'>热销推荐</div>
+    <ul>
+      <li class="item border-bottom" v-for="(item) in recommendList" :key="item.id">
+        <div class='item-img-wrapper'>
+          <img class='item-img' :src="item.imgUrl"/>
+        </div>
+        <div class='item-info'>
+          <p class='item-title'>{{item.title}}</p>
+          <p class='item-desc'>{{item.desc}}</p>
+          <button class='item-button'>查看详情</button>
+        </div>
+      </li>
+    </ul>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeRecommend',
+  data () {
+    return {
+      recommendList: [
+        {
+          id: '001',
+          imgUrl: 'http://img1.qunarzz.com/sight/201405/27/4e1c7943ceb94759c8d65eac.jpg_150x150_1b5d04b2.jpg',
+          title: '大明湖',
+          desc: 'AAAA景区'
+        },
+        {
+          id: '002',
+          imgUrl: 'http://img1.qunarzz.com/sight/201405/27/4e1c7943ceb94759c8d65eac.jpg_150x150_1b5d04b2.jpg',
+          title: '大明湖',
+          desc: 'AAAA景区'
+        },
+        {
+          id: '003',
+          imgUrl: 'http://img1.qunarzz.com/sight/201405/27/4e1c7943ceb94759c8d65eac.jpg_150x150_1b5d04b2.jpg',
+          title: '大明湖',
+          desc: 'AAAA景区'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~@/assets/styles/mixins.styl'
+  .recommend-title
+    margin-top: .2rem
+    line-height: .8rem
+    background: #eee
+    text-indent: .2rem
+  .item
+    overflow: hidden
+    display:flex
+    height: 1.9rem
+    // background: red
+    .item-img
+      width: 1.7rem
+      height: 1.7rem
+      padding: .1rem
+    .item-info
+      flex: 1
+      padding: .1rem
+      min-width: 0
+      .item-title
+        line-height: .54rem
+        font-size: .32rem
+        ellipsis()
+      .item-desc
+        line-height: .4rem
+        color: #ccc
+        ellipsis()
+      .item-button
+        line-height: .44rem
+        margin-top: .16rem
+        background: #ff9300
+        padding: 0 .2rem
+        border-radius: .06rem
+        color: #fff
+
+</style>
